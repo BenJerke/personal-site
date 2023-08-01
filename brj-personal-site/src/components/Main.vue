bdbed69062d52950bf58f7411571342b6c1c02fd
Ben Jerke <benj427@gmail.com>
2023-08-01 09:26:06 -0400

    Gonna try something dumb.

--- brj-personal-site/src/components/Main.vue
+++ brj-personal-site/src/components/Main.vue
@@ -2,21 +2,8 @@
       <main> 
           <h1>Main Content Here</h1>
           <section>
-              <md-block src="">
-
-              </md-block>
-              <!-- 
-                  This is where the stuff goes, once I figure out what it is.
-                  Okay - let's say this is where I'd show a blog post. 
-                  I'd need it to: 
-                  - get a markdown file for the given post. 
-                  - render markdown files somehow. 
-                    - Figure out a way to put workable links inside markdown files (should be easy enough, right? it's all just posts, innit?)  
-                  - scroll independently from the rest of the content on screen. 
-
-
-               -->
-
+              <!-- <md-block src="../../test/test.md"></md-block>
+                  TODO: Bind src to actual data source -->
           </section>
       </main>
 </template>
